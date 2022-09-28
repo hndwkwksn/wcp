05d90ea45886f67ff7f3ea84f03de1d140331368
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-28 23:53:48 +0000

    [Finish]Chapter04

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        String name = "本田湧";
+        System.out.println(name);
+
+        name = "田中";
+        System.out.println(name);
+
+        final int LUCKY_NUMBER = 7;
+        System.out.println(LUCKY_NUMBER);
+    }
+}
