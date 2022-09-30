af66d8869fdcd0a4847ca4465136d8d4df0e9434
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-30 02:51:08 +0000

    [Finish]test

---
+++ StringOperation.java
@@ -0,0 +1,8 @@
+public class StringOperation {
+    public static void main(String[] args) {
+        String a = "applePie";
+        String b = "applePie";
+        // String c = b + "Pie";
+        c = a == b;
+    }
+}
